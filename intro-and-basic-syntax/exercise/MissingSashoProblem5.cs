d8f5de83e03c96567c35b88aac75d0287546fa97
KostaNikolov <kocta616@gmail.com>
2018-03-17 18:59:52 +0200

    Changed variable names health and energy to currHealth and currEnergy

--- intro-and-basic-syntax/exercise/MissingSashoProblem5.cs
+++ intro-and-basic-syntax/exercise/MissingSashoProblem5.cs
@@ -7,14 +7,14 @@ namespace CharacterStats
         static void Main()
         {
             string name = Console.ReadLine();
-            int health = int.Parse(Console.ReadLine());
+            int currHealth = int.Parse(Console.ReadLine());
             int maxHealth = int.Parse(Console.ReadLine());
-            int energy = int.Parse(Console.ReadLine());
+            int currEnergy = int.Parse(Console.ReadLine());
             int maxEnergy = int.Parse(Console.ReadLine());
 
             Console.WriteLine($"Name: {name}");
-            Console.WriteLine($"Health: |{new string('|',health)}{new string('.',maxHealth-health)}|");
-            Console.WriteLine($"Energy: |{new string('|', energy)}{new string('.', maxEnergy - energy )}|");
+            Console.WriteLine($"Health: |{new string('|', currHealth)}{new string('.', maxHealth - currHealth)}|");
+            Console.WriteLine($"Energy: |{new string('|', currEnergy)}{new string('.', maxEnergy - currEnergy)}|");
 
         }
     }
