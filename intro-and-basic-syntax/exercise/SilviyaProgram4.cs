969b7ec152a76f05a42d3691b84d16369b7dbedf
Tsveti <ThunderStorm@students.softuni.bg>
2018-03-17 21:00:17 +0200

    Adding comments to problem4.cs

--- intro-and-basic-syntax/exercise/SilviyaProgram4.cs
+++ intro-and-basic-syntax/exercise/SilviyaProgram4.cs
@@ -10,10 +10,16 @@ namespace HexadecimalConvert
     {
         static void Main(string[] args)
         {
-
+            //Console input
             var hexNumber = Console.ReadLine();
+
+            //We make new string from second index to the end of inserted number
             var newHexNumber = hexNumber.Substring(2);
+
+            //Convert the string to integer base ten
             var baseTenNumber = Convert.ToInt32(newHexNumber, 16);
+
+            //Print the base ten number
             Console.WriteLine(baseTenNumber);
         }
     }
