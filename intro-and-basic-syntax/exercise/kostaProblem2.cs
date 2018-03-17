adbfbfc241352a28b2623a0718f3d2d754f75e00
kratun <mitevdv@abv.bg>
2018-03-17 17:20:31 +0200

    Problem 2 commented by kratun because Aleksander missing Problem 3 commented by kratun Add exercise tasks file .docx

--- intro-and-basic-syntax/exercise/kostaProblem2.cs
+++ intro-and-basic-syntax/exercise/kostaProblem2.cs
@@ -6,10 +6,12 @@ namespace _02._Rectangle_area
     {
         static void Main(string[] args)
         {
+            // We receive from the Console Rectangle side "a" and "b" 
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
-
+            //Calculate rectangle area
             double area = a * b;
+            //Print result
             Console.WriteLine($"{area:f2}");
         }
     }
