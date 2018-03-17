8b48ada4d6f162bed54399b296988d0cb876db5e
cypetrova <37227431+cypetrova@users.noreply.github.com>
2018-03-17 18:19:50 +0200

    Add files via upload

---
+++ intro-and-basic-syntax/exercise/cvetiProblem3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Miles_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miles = double.Parse(Console.ReadLine());
+            var kilometers = miles * 1.60934;
+            Console.WriteLine("{0:F2}", kilometers);
+        }
+    }
+}
