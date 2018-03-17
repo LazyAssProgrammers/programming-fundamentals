fac118c38aeee108e7e405b27e7b5d353e2a2dc2
Silviya Mircheva <mircheva.silviya@gmail.com>
2018-03-17 18:43:31 +0200

    changed variable name in problem 2; documented problem 5

--- intro-and-basic-syntax/exercise/MissingSashoProblem5.cs
+++ intro-and-basic-syntax/exercise/MissingSashoProblem5.cs
@@ -6,12 +6,14 @@ namespace CharacterStats
     {
         static void Main()
         {
+            // read the name, current and max stats from the console - each or a separate line
             string name = Console.ReadLine();
             int health = int.Parse(Console.ReadLine());
             int maxHealth = int.Parse(Console.ReadLine());
             int energy = int.Parse(Console.ReadLine());
             int maxEnergy = int.Parse(Console.ReadLine());
 
+            // print the name and visualise the stats
             Console.WriteLine($"Name: {name}");
             Console.WriteLine($"Health: |{new string('|',health)}{new string('.',maxHealth-health)}|");
             Console.WriteLine($"Energy: |{new string('|', energy)}{new string('.', maxEnergy - energy )}|");
