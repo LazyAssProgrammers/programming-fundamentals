334095cdb1282bc4bf6ee395212461c420009e37
cypetrova <37227431+cypetrova@users.noreply.github.com>
2018-03-18 22:53:12 +0200

    Update DragoProblem1.cs

--- intro-and-basic-syntax/exercise/DragoProblem1.cs
+++ intro-and-basic-syntax/exercise/DragoProblem1.cs
@@ -8,12 +8,12 @@ namespace DebitCardNumber
         static void Main()
         {
             // We received four numbers from Console 
-            int firstFourDigits=int.Parse(Console.ReadLine());
+            int firstFourNumbers=int.Parse(Console.ReadLine());
             int secondFourDigits = int.Parse(Console.ReadLine());
             int thirdFourDigits = int.Parse(Console.ReadLine());
             int forthFourDigits = int.Parse(Console.ReadLine());
             //Prints received numbers in 4-digit debit card format
-            Console.WriteLine($"{firstFourDigits:D4} {secondFourDigits:D4} {thirdFourDigits:D4} {forthFourDigits:D4}");
+            Console.WriteLine($"{firstFourNumbers:D4} {secondFourDigits:D4} {thirdFourDigits:D4} {forthFourDigits:D4}");
             
         }
     }
